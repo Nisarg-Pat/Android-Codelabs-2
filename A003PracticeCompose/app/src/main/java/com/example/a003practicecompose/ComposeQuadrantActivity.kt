ef4f469b4c08cfba3d00adf2992b9f2e5c4c3700
npatelmm <npatel@metromobility.io>
2023-12-08 13:16:14 -0500

    A003: Practice 3 Completed

--- A003PracticeCompose/app/src/main/java/com/example/a003practicecompose/ComposeQuadrantActivity.kt
+++ A003PracticeCompose/app/src/main/java/com/example/a003practicecompose/ComposeQuadrantActivity.kt
@@ -3,13 +3,24 @@ package com.example.a003practicecompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.a003practicecompose.ui.theme.A003PracticeComposeTheme
 
 class ComposeQuadrantActivity : ComponentActivity() {
@@ -22,7 +33,7 @@ class ComposeQuadrantActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    ComposeQuadrant()
+                    ComposeQuadrants()
                 }
             }
         }
@@ -31,14 +42,78 @@ class ComposeQuadrantActivity : ComponentActivity() {
 
 
 @Composable
-fun ComposeQuadrant(modifier: Modifier = Modifier) {
-    Text(text = "Compose Quadrant")
+fun ComposeQuadrants(modifier: Modifier = Modifier) {
+    Column {
+        Row(
+            modifier = modifier.weight(1F)
+        ) {
+            ComposeQuadrant(
+                backgroundColor = Color(0xFFEADDFF),
+                title = stringResource(id = R.string.text_composable),
+                description = stringResource(id = R.string.text_composable_text),
+                modifier = Modifier.weight(1F)
+            )
+            ComposeQuadrant(
+                backgroundColor = Color(0xFFD0BCFF),
+                title = stringResource(id = R.string.image_composable),
+                description = stringResource(id = R.string.image_composable_text),
+                modifier = Modifier.weight(1F)
+            )
+        }
+        Row(
+            modifier = modifier.weight(1F)
+        ) {
+            ComposeQuadrant(
+                backgroundColor = Color(0xFFB69DF8),
+                title = stringResource(id = R.string.row_composable),
+                description = stringResource(id = R.string.row_composable_text),
+                modifier = Modifier.weight(1F)
+            )
+            ComposeQuadrant(
+                backgroundColor = Color(0xFFF6EDFF),
+                title = stringResource(id = R.string.column_composable),
+                description = stringResource(id = R.string.column_composable_text),
+                modifier = Modifier.weight(1F)
+            )
+        }
+    }
+
+}
+
+@Composable
+fun ComposeQuadrant(
+    backgroundColor: Color,
+    title: String,
+    description: String,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier
+            .background(backgroundColor)
+            .padding(16.dp)
+            .fillMaxSize()
+    ) {
+        Text(
+            text = title,
+            fontWeight = FontWeight.Bold,
+            color = Color.Black,
+            modifier = Modifier
+                .padding(bottom = 16.dp)
+        )
+        Text(
+            text = description,
+            color = Color.Black,
+            textAlign = TextAlign.Justify
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun ComposeQuadrantPreview() {
     A003PracticeComposeTheme {
-        ComposeQuadrant()
+        ComposeQuadrants()
     }
 }
