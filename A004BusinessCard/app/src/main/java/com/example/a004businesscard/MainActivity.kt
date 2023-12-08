39d2f94e2ec08311bb8cf4a18272941508580ff1
npatelmm <npatel@metromobility.io>
2023-12-08 15:23:57 -0500

    A004: Init

---
+++ A004BusinessCard/app/src/main/java/com/example/a004businesscard/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.example.a004businesscard
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.a004businesscard.ui.theme.A004BusinessCardTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            A004BusinessCardTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    BusinessCard("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun BusinessCard(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun BusinessCardPreview() {
+    A004BusinessCardTheme {
+        BusinessCard("Android")
+    }
+}
