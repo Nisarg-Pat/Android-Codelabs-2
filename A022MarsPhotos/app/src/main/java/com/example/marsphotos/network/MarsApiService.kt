ae91f554d1e92ae9ec76e52ea602f530b094319f
Nisarg Patel <patel321nisarg@gmail.com>
2024-08-07 12:40:14 -0400

    A022 MarsPhotos: Add repository and Manual Dependency Injection

--- A022MarsPhotos/app/src/main/java/com/example/marsphotos/network/MarsApiService.kt
+++ A022MarsPhotos/app/src/main/java/com/example/marsphotos/network/MarsApiService.kt
@@ -16,22 +16,8 @@
 
 package com.example.marsphotos.network
 
-import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
-import kotlinx.serialization.json.Json
-import okhttp3.MediaType.Companion.toMediaType
-import retrofit2.Retrofit
+import com.example.marsphotos.model.MarsPhoto
 import retrofit2.http.GET
-
-private const val BASE_URL =
-    "https://android-kotlin-fun-mars-server.appspot.com"
-
-/**
- * Use the Retrofit builder to build a retrofit object using a kotlinx.serialization converter
- */
-private val retrofit = Retrofit.Builder()
-    .addConverterFactory(Json.asConverterFactory("application/json".toMediaType()))
-    .baseUrl(BASE_URL)
-    .build()
 
 /**
  * Retrofit service object for creating api calls
@@ -41,11 +27,3 @@ interface MarsApiService {
     suspend fun getPhotos(): List<MarsPhoto>
 }
 
-/**
- * A public Api object that exposes the lazy-initialized Retrofit service
- */
-object MarsApi {
-    val retrofitService: MarsApiService by lazy {
-        retrofit.create(MarsApiService::class.java)
-    }
-}
